97e1d6ab189092ae5d56ce866392924b1c214605
Tine Kleivane <kleivane@gmail.com>
2014-01-12 20:12:41 +0100

    Connect til mongodb

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var request = require('request');
 var path = require('path');
 var consolidate = require('consolidate');
 
+var db = require('./lib/db');
 var routing = require('./routing');
 var config = require('./lib/config')();
 
@@ -16,4 +17,14 @@ app.set('views', path.join(__dirname, 'views'));
 
 routing.setup(app);
 
-app.listen(config.port);
+
+db.connect(config.db, function(err){
+	if (err) {
+    	console.log(err);
+  	}
+
+  	console.log('Connected to ' + config.db.url);
+
+	app.listen(config.port);
+	console.log('Express app listening at port ' + config.port);
+});
