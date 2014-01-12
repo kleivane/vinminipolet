7dc0037c5fef9f5347eb17383e349cc8b2b441c8
Tine Kleivane <kleivane@gmail.com>
2014-01-12 17:35:57 +0100

    Startet å gjøre klar til lagring av viner i DB

--- routing.js
+++ routing.js
@@ -1,11 +1,8 @@
 var wineView = require('./lib/wine-view');
+var wineResource = require('./lib/wine-resource');
 
 exports.setup = function (app) {
 	app.get('/', wineView.list);
-
 	app.get('/new', wineView.newWine);
-
-	app.post('/new', function(req, res) {	  
-	  res.redirect('/');
-	});
+	app.post('/new', wineResource.insert);
 };
