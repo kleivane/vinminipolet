db9a0559750b0bf61c562c5b83b631ffbb3b27a3
Tine Kleivane <kleivane@gmail.com>
2014-01-12 15:44:17 +0100

    Lagt til inputview

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var request = require('request');
+var path = require('path');
+var consolidate = require('consolidate');
 
 var port = process.env.PORT || 1339;
 
 var app = express();
 
+app.engine("html", consolidate.handlebars);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', function(req, res) {
   res.json("Get ready for vines!");
+});
+
+app.get('/new', function(req, res) {
+  res.render('layout', {
+    title: 'Legg inn en ny vin',
+    partials: { content: '_new' }
+  });
 });
 
 app.listen(port);
