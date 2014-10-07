17db2e3c56a70e2f1a24a74e784f16c1a44b0f59
Tine Kleivane <kleivane@gmail.com>
2014-10-07 21:38:18 +0200

    Oppdatert til enklere views. Poster full jsonstruktur ned i databasen

--- routing.js
+++ routing.js
@@ -1,8 +1,10 @@
+var bodyParser = require('body-parser');
+
 var wineView = require('./lib/wine/wine-view');
 var wineResource = require('./lib/wine/wine-resource');
 
 exports.setup = function (app) {
 	app.get('/', wineView.list);
 	app.get('/new', wineView.newWine);
-	app.post('/new', wineResource.insert);
+	app.post('/new', bodyParser.urlencoded(), wineResource.insert);
 };
